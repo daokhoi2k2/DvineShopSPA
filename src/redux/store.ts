5b9fba3f469111e6688ea70efe89ddbfc0878b86
daokhoi2k2 <daoducminhkhoi@gmail.com>
2022-04-02 06:56:12 +0700

    Update redux developer tools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,29 @@
-import { compose, createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import {rootReducer} from "./reducers";
 
 // Use Redux Developer with Typescript
 declare global {
     interface Window {
       __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+      __REDUX_DEVTOOLS_EXTENSION__?: typeof compose;
     }
 }
 
-// initialState
-const initialState = {};
+// preloadedState tham số thứ 2 của createStore sẽ có độ ưu tiện cao hơn initialState trong Reducer
+// Docs: https://redux.js.org/usage/structuring-reducers/initializing-state
+// const initialState = {
+//   config: {
+//     isNavDrawer: false,
+//     isAdminNavDrawer: true,
+//     AuthModalBox: {
+//       isShow: false,
+//       boxName: undefined,
+//     },
+//   }
+// };
+
 
 // Create store
-const store = createStore(rootReducer, initialState);
+const store = createStore(rootReducer, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
