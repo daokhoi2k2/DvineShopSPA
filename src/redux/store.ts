1a40a0cd37c1b49cac375194e887d75dba129493
daokhoi2k2 <daoducminhkhoi@gmail.com>
2022-04-04 02:40:10 +0700

    Update redux thunk

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,13 @@
 import { applyMiddleware, compose, createStore } from "redux";
-import { getAllCategoryService } from "services/category";
-import { getAllCategoriesSuccess } from "./actions/category";
-import { setAuthModalBox } from "./actions/config";
-import {rootReducer} from "./reducers";
-import { GET_ALL_CATEGORIES } from "./types/category";
+import { rootReducer } from "./reducers";
+import thunk from "redux-thunk"
 
 // Use Redux Developer with Typescript
 declare global {
-    interface Window {
-      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-      __REDUX_DEVTOOLS_EXTENSION__?: typeof compose;
-    }
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    __REDUX_DEVTOOLS_EXTENSION__?: typeof compose;
+  }
 }
 
 // preloadedState tham số thứ 2 của createStore sẽ có độ ưu tiện cao hơn initialState trong Reducer
@@ -30,23 +27,22 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // Dùng 1 action khác để dispatch lấy kết quả đế tránh vòng lặp vô hạn khi
 // dispatch => middleware => dispatch lại cái cũ => middleware , .....
-const handleAsyncLogic = (store:any) => {
-  return (next:any) => {
-    return async (action: any) => {
-      if(typeof action === 'function') {
-          // Next sẽ là store.dispatch nếu đây là middleware cuối cùng 
-          // Nên dùng action(next) vì có thể còn có middleware ở phía sau nếu dùng action(store.distpach) sẽ bỏ qua tất cả middleware ở phía dưới
-          return action(next)
-      }
+// const handleAsyncLogic = (store: any) => {
+//   return (next: any) => {
+//     return async (action: any) => {
+//       if (typeof action === "function") {
+//         // Next sẽ là store.dispatch nếu đây là middleware cuối cùng
+//         // Nên dùng action(next) vì có thể còn có middleware ở phía sau nếu dùng action(store.distpach) sẽ bỏ qua tất cả middleware ở phía dưới
+//         return action(next);
+//       }
 
-      return next(action);
-    }
-  }
-}
+//       return next(action);
+//     };
+//   };
+// };
 
-
-const middlewareEnchancer = applyMiddleware(handleAsyncLogic);
-
+// const middlewareEnchancer = applyMiddleware(handleAsyncLogic);
+const middlewareEnchancer = applyMiddleware(thunk);
 
 // Create store
 const store = createStore(rootReducer, {}, composeEnhancers(middlewareEnchancer));
